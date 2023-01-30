749e0c58fe39e715d37f72e26bb5615012c2ed26
Muhammadshoh Rajabov <rajabovmuhammadshoh009@gmail.com>
2023-01-30 20:31:13 +0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <stored-resources
+    v-for="resource in storedResources"
+    :key="resource.id"
+    :title="resource.title"
+    :description="resource.description"
+    :link="resource.link"
+  ></stored-resources>
+</template>
+
+<script>
+import StoredResources from './components/StoredResources.vue';
+
+export default {
+  components: { StoredResources },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation ',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https:/google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/StoredResources.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul>
+    <li>
+      <h1>{{ title }}</h1>
+      <h2>{{ description }}</h2>
+      <a :href="link">View Resource</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ['title', 'description', 'link'],
+};
+</script>
